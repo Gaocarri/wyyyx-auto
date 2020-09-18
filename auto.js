9b4cffc7d654176351cfc00d7b0eb3096f4162a6
GaoCarri <yajie@promote.cache-dns.local>
2020-09-18 18:17:18 +0800

    autojs

--- auto.js
+++ auto.js
@@ -1,3 +1,4 @@
+// 排队连点
 timer = setInterval(() => {
     wyyouxi.click()
 }, 500)
@@ -7,3 +8,15 @@ setInterval(() => {
         wyyouxi.click()
     }, 500)
 }, 10000)
+
+// 早上六点自动排队
+function auto() {
+    let date = new Date()
+    if (date.getHours() === 6) {
+        wyyouxi.click()
+        window.clearInterval(timeId)
+    }
+}
+let timeId = setInterval(() => {
+    auto()
+}, 300000)
